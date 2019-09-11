1490c2827b00e288cbe5eea1606ba45683ab217b
FabyP <fabienne.pipping@gmail.com>
2019-09-11 16:32:22 +0200

    Accessibility Test

--- src/filterbar/FilterBar.js
+++ src/filterbar/FilterBar.js
@@ -55,16 +55,18 @@ class FilterBar extends Component {
                 <ui5-title level="H3">Events</ui5-title>
 
                 <div className="events-page-filter-bar-actions">
-                    <form role="search">
-                        <ui5-label class="hidden" for="searchInput">Eingabefeld Suche</ui5-label>
-                        <ui5-input id="searchInput" aria-label="Suche" class="events-page-searchfield" placeholder="Suche" ref={this.searchInput}>
-                            <ui5-icon slot="icon" src="sap-icon://search"></ui5-icon>
-                        </ui5-input>
-                    </form>
+                    <ui5-label class="hidden" for="searchInput">Eingabefeld Suche</ui5-label>
+                    <ui5-input id="searchInput" aria-label="Suche" class="events-page-searchfield" placeholder="Suche" ref={this.searchInput}>
+                        <ui5-icon slot="icon" src="sap-icon://search"></ui5-icon>
+                    </ui5-input>
 
-                    <ui5-button role ="button" onClick={this.openDialog.bind(this)} design="Transparent" title="Event erstellen" aria-label="Event erstellen">Erstellen</ui5-button>
-                    <ui5-button class="events-page-filter-bar-overflow" onClick={this.props.sortDesc.bind(this)} icon="sap-icon://sort-descending" design="Transparent" title="Absteigend nach Datum sortieren" aria-label="Absteigend nach Datum sortieren"></ui5-button>
-                    <ui5-button class="events-page-filter-bar-overflow" onClick={this.props.sortAsc.bind(this)} icon="sap-icon://sort-ascending" design="Transparent" title="Aufsteigend nach Datum sortieren" aria-label="Aufsteigend nach Datum sortieren"></ui5-button>
+                    <ui5-button role="button" onClick={this.openDialog.bind(this)} design="Transparent" title="Event erstellen" aria-label="Event erstellen">Erstellen</ui5-button>
+                    <ui5-button class="events-page-filter-bar-overflow" onClick={this.props.sortDesc.bind(this)} icon="sap-icon://sort-descending" design="Transparent" title="Absteigend nach Datum sortieren" aria-label="Absteigend nach Datum sortieren">
+                        <span class="hidden">Absteigend nach Datum sortieren</span>
+                    </ui5-button>
+                    <ui5-button class="events-page-filter-bar-overflow" onClick={this.props.sortAsc.bind(this)} icon="sap-icon://sort-ascending" design="Transparent" title="Aufsteigend nach Datum sortieren" aria-label="Aufsteigend nach Datum sortieren">
+                        <span class="hidden">Aufsteigend nach Datum sortieren</span>
+                    </ui5-button>
                 </div>
 
                 <ui5-dialog header-text="Neues Event erstellen" ref={this.dialog}>
